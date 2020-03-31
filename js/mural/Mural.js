00643098577243020463eba1cddcc0a9f5f74bdc
AzraelGarden <azraelgarden@gmail.com>
2020-03-31 14:31:11 -0300

    Atualização 2.0

--- js/mural/Mural.js
+++ js/mural/Mural.js
@@ -1,19 +1,58 @@
 const Mural = (function(_render, Filtro){
     "use strict"
-    let cartoes = []
+
+    let cartoes = pegaCartoesUsuario()
+
     const render = () => _render({cartoes: cartoes, filtro: Filtro.tagsETexto});
+    render()
 
     Filtro.on("filtrado", render)
+
+    function preparaCartao(cartao){
+        cartao.on("mudanca.**", salvaCartoes)
+        cartao.on("remocao", ()=>{
+            cartoes = cartoes.slice(0)
+            cartoes.splice(cartoes.indexOf(cartao),1)
+            salvaCartoes()
+            render()
+        })
+    }
+
+    function pegaCartoesUsuario(){
+        let cartoesLocal = JSON.parse(localStorage.getItem(usuario))
+        if(cartoesLocal){
+            return cartoesLocal.map(cartaoLocal => {
+                let cartao = new Cartao(cartaoLocal.conteudo, cartaoLocal.tipo)
+                preparaCartao(cartao)
+                return cartao
+            })
+        } else {
+            return []
+        }
+    }
+
+    function salvaCartoes (){
+        localStorage.setItem(usuario, JSON.stringify(
+            cartoes.map(cartao => ({conteudo: cartao.conteudo, tipo: cartao.tipo}))
+        ))
+    }
+
+    login.on("login", ()=>{
+        cartoes = pegaCartoesUsuario()
+        render()
+    })
+
+    login.on("logout", ()=> {
+        cartoes = []
+        render()
+    })
 
     function adiciona(cartao){
         if(logado){
             cartoes.push(cartao)
+            salvaCartoes()
             cartao.on("mudanca.**", render)
-            cartao.on("remocao", ()=>{
-                cartoes = cartoes.slice(0)
-                cartoes.splice(cartoes.indexOf(cartao),1)
-                render()
-            })
+            preparaCartao(cartao)
             render()
             return true
         } else {
